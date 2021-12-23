4571bb36f1b02e6e71b61f65bf9e6964d3963e02
Piszmog <piszmogcode@gmail.com>
2021-12-22 20:55:30 -0700

    upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-    purge: {
-        enabled: !process.env.ROLLUP_WATCH,
-        content: ['./public/index.html', './src/**/*.svelte'],
-        options: {
-            defaultExtractor: content => [
-                ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
-                ...(content.match(/(?<=class:)[^=>\/\s]*/g) || []),
-            ],
-        },
-    },
-    darkMode: false, // or 'media' or 'class'
+    content: ['./public/index.html', './src/**/*.svelte'],
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [],
